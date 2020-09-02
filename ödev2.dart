7b4c88818d930616fa632e1bd499d2beec46a93c
Talha Turgut <talhaturgut@windowslive.com>
2020-09-03 00:21:28 +0300

    Update ödev2.dart

--- ödev2.dart
+++ ödev2.dart
@@ -18,6 +18,24 @@ toplam +=sayi;
 while(sayi%2 != 0){
   print("toplam=$toplam");
 }
+  
+  /*
+  import 'dart:io';
+
+void main(List<String> args) {
+  int sayi = 0, toplam = 0;
+  do {
+    print("sayi gir:");
+    String a = stdin.readLineSync();
+    sayi = int.parse(a);
+    if (sayi % 2 != 0) break;
+    toplam += sayi;
+    print("toplam: $toplam");
+  } while (sayi % 2 == 0);
+
+  print("Toplam: $toplam");
+}
+*/
 
 
 
